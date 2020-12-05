66380f779a5325d828b1c62bf20cc6baae11999c
Pin-Yu Wang <pywang@datalab.cs.nthu.edu.tw>
2020-12-06 00:53:56 +0800

    Aggregate code

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -7,10 +7,10 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let file_name = &args[1];
 
-    let input_vec = read_lines_to_vec(file_name);
-    let result = solve(&input_vec.unwrap());
+    let input_vec = read_lines_to_vec(file_name).unwrap();
 
-    println!("{}", result.unwrap());
+    println!("part1 result: {}", solve_part1(&input_vec).unwrap());
+    println!("part2 result: {}", solve_part2(&input_vec).unwrap());
 }
 
 pub fn read_lines_to_vec(file_name: &str) -> Result<Vec<i32>, Error> {
@@ -26,7 +26,7 @@ pub fn read_lines_to_vec(file_name: &str) -> Result<Vec<i32>, Error> {
     Ok(vec)
 }
 
-fn solve(input_vec: &[i32]) -> Option<i32> {
+fn solve_part1(input_vec: &[i32]) -> Option<i32> {
     let mut map: HashMap<i32, usize> = HashMap::new();
 
     // this problem is simliar to leet code two sum
@@ -44,13 +44,36 @@ fn solve(input_vec: &[i32]) -> Option<i32> {
     None
 }
 
+fn solve_part2(input_vec: &[i32]) -> Option<i32> {
+    for num1 in input_vec.iter() {
+        for num2 in input_vec.iter() {
+            if num2 == num1 {
+                continue;
+            }
+
+            let residual = 2020 - num1 - num2;
+            if input_vec.contains(&residual) {
+                return Some(residual * num1 * num2);
+            }
+        }
+    }
+
+    None
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn test_solve() {
+    fn test_solve_part1() {
         let numbers = vec![1721, 979, 366, 299, 675, 1456];
-        assert_eq!(solve(&numbers).unwrap(), 514579);
+        assert_eq!(solve_part1(&numbers).unwrap(), 514579);
+    }
+
+    #[test]
+    fn test_solve_part2() {
+        let numbers = vec![1721, 979, 366, 299, 675, 1456];
+        assert_eq!(solve_part2(&numbers).unwrap(), 241861950);
     }
 }
--- src/bin/day02.rs
+++ src/bin/day02.rs
@@ -9,12 +9,12 @@ fn main() {
 
     let input_vec = read_lines_to_vec(file_name).unwrap();
 
-    let result = solve(&input_vec).unwrap();
-    println!("{}", result);
+    println!("part1 result: {}", solve_part1(&input_vec).unwrap());
+    println!("part2 result: {}", solve_part2(&input_vec).unwrap());
 }
 
 pub fn read_lines_to_vec(file_name: &str) -> Result<Vec<String>, Error> {
-    let input = File::open(file_name).unwrap();
+    let input = File::open(file_name)?;
     let buffered = BufReader::new(input);
 
     let mut vec = Vec::new();
@@ -26,7 +26,28 @@ pub fn read_lines_to_vec(file_name: &str) -> Result<Vec<String>, Error> {
     Ok(vec)
 }
 
-pub fn solve(input_vec: &[String]) -> Option<i32> {
+pub fn solve_part1(input_vec: &[String]) -> Option<i32> {
+    let re = Regex::new(r"(\d+)-(\d+) (\w): (\w+)").unwrap();
+
+    let mut counter: i32 = 0;
+
+    for line in input_vec.iter() {
+        let caps = re.captures(line).unwrap();
+
+        let min: usize = caps[1].parse().unwrap();
+        let max: usize = caps[2].parse().unwrap();
+        let ch = &caps[3];
+        let text = &caps[4];
+
+        let contain_times = text.matches(ch).count();
+        if contain_times >= min && contain_times <= max {
+            counter += 1;
+        }
+    }
+    Some(counter)
+}
+
+pub fn solve_part2(input_vec: &[String]) -> Option<i32> {
     let re = Regex::new(r"(\d+)-(\d+) (\w): (\w+)").unwrap();
 
     let mut counter: i32 = 0;
@@ -59,12 +80,22 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_solve() {
+    fn test_solve_part1() {
         let lines: Vec<String> = vec!["1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc"]
             .into_iter()
             .map(|s| s.to_owned())
             .collect();
 
-        assert_eq!(solve(&lines).unwrap(), 1);
+        assert_eq!(solve_part1(&lines).unwrap(), 2);
+    }
+
+    #[test]
+    fn test_solve_part2() {
+        let lines: Vec<String> = vec!["1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc"]
+            .into_iter()
+            .map(|s| s.to_owned())
+            .collect();
+
+        assert_eq!(solve_part2(&lines).unwrap(), 1);
     }
 }
