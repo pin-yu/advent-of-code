f473c7d20a53f406c6ece40c6a17b89555461217
Pin-Yu Wang <pywang@datalab.cs.nthu.edu.tw>
2020-12-08 20:31:17 +0800

    Finish day05

--- src/bin/day05.rs
+++ src/bin/day05.rs
@@ -10,6 +10,7 @@ fn main() {
 
     let input_vec = read_lines_to_vec(&file_name).unwrap();
     println!("part1 result: {}", solve_part1(&input_vec).unwrap());
+    println!("part2 result: {}", solve_part2(&input_vec).unwrap());
 }
 
 fn read_lines_to_vec(file_name: &String) -> Result<Vec<String>, Error> {
@@ -34,6 +35,18 @@ fn solve_part1(input_vec: &[String]) -> Option<i32> {
     }
 
     Some(max_seat_id)
+}
+
+fn solve_part2(input_vec: &[String]) -> Option<i32> {
+    let mut seats: Vec<i32> = Vec::new();
+    for text in input_vec.iter() {
+        let row = get_row(text);
+        let col = get_col(text);
+        let seat_id = row * 8 + col;
+        seats.push(seat_id);
+    }
+
+    find_vacancy_seat(seats)
 }
 
 fn get_row(text: &String) -> i32 {
@@ -62,11 +75,27 @@ fn divider(
         } else if ch == upper_char {
             lower_bound += cut_point;
         } else {
-            panic!("first seven characters are invalid!")
+            panic!("invalid input!")
         }
     }
     // eventually, lrb will equal urb
     lower_bound
+}
+
+fn find_vacancy_seat(mut seats: Vec<i32>) -> Option<i32> {
+    seats.sort();
+
+    let mut current_seat_id = seats[0];
+
+    // find the lack seat id
+    for &v in seats.iter() {
+        if v != current_seat_id {
+            return Some(current_seat_id);
+        }
+        current_seat_id += 1;
+    }
+
+    None
 }
 
 #[cfg(test)]
@@ -81,5 +110,15 @@ mod tests {
             .collect();
 
         assert_eq!(solve_part1(&vec).unwrap(), 820);
+    }
+
+    #[test]
+    fn test_solve_part2() {
+        let vec: Vec<String> = vec!["FFFFFFFLLL", "FFFFFFFLLR", "FFFFFFFLRL", "FFFFFFFRLL"]
+            .into_iter()
+            .map(|s| s.to_owned())
+            .collect();
+
+        assert_eq!(solve_part2(&vec).unwrap(), 3);
     }
 }
